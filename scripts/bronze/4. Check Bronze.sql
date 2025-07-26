ff16bdbeefa1d36cfab21806648193e544203a80
Mar San Pedro <127751590+jmmsanpedro@users.noreply.github.com>
2025-07-26 11:17:56 +0800

    Create 4. Check Bronze.sql

---
+++ scripts/bronze/4. Check Bronze.sql
@@ -0,0 +1,11 @@
+use role sysadmin;
+use database sales;
+
+select * from bronze.crm_cust_info;
+select * from bronze.crm_prd_info;
+select * from bronze.crm_sales_details;
+
+select * from bronze.erp_cust_az12;
+select * from bronze.erp_px_cat_g1v2;
+select * from bronze.erp_stores;
+
