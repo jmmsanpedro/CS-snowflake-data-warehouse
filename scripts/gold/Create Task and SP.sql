85d70ae3a2dbe230f7f3ff7ac2da692747705377
Mar San Pedro <127751590+jmmsanpedro@users.noreply.github.com>
2025-07-29 16:05:55 +0800

    Create Create Task and SP.sql

---
+++ scripts/gold/Create Task and SP.sql
@@ -0,0 +1,105 @@
+USE DATABASE SALES;
+USE SCHEMA GOLD;
+USE ROLE SYSADMIN;
+
+
+CREATE OR REPLACE TASK merge_gold_task
+WAREHOUSE = COMPUTE_WH
+SCHEDULE = '1 minute'
+AS
+CALL GOLD.SP_LOAD_DIM_CUSTOMER();
+
+execute task merge_gold_task;
+CREATE OR REPLACE PROCEDURE GOLD.SP_LOAD_DIM_CUSTOMER()
+    RETURNS STRING
+    LANGUAGE SQL
+    AS $$
+    BEGIN 
+    MERGE INTO SALES.GOLD.DIM_CUSTOMER AS TGT 
+    USING SALES.GOLD.VW_NEW_CUSTOMER_SRC AS SRC 
+    ON TGT.CUSTOMER_ID = SRC.CST_ID AND
+    TGT.DWH_STATUS = SRC.DWH_STATUS
+        
+    WHEN MATCHED
+    AND (
+        TGT.CUSTOMER_NUMBER <> SRC.CST_KEY
+        OR TGT.FIRST_NAME <> SRC.CST_FIRSTNAME
+        OR TGT.LAST_NAME <> SRC.CST_LASTNAME
+        OR TGT.BIRTH_DATE <> SRC.BDATE
+        OR TGT.MARITAL_STATUS <> SRC.CST_MARITAL_STATUS
+        OR TGT.GENDER <> SRC.CST_GNDR_derived
+        OR TGT.CREATE_DATE <> SRC.CST_CREATE_DATE
+        OR TGT.LOYALTY_JOIN_DATE <> SRC.CST_LOYALTY_JOIN_DATE
+        OR TGT.STORE_KEY <> SRC.STORE_KEY
+    ) 
+    THEN UPDATE
+    SET
+    TGT.DWH_STATUS = 'Expired',
+    TGT.DWH_END_DATE = GETDATE() -- VARIABLE TO LOAD DATE
+    
+    WHEN NOT MATCHED 
+    THEN INSERT
+        (
+            CUSTOMER_ID,
+            CUSTOMER_NUMBER,
+            FIRST_NAME,
+            LAST_NAME,
+            BIRTH_DATE,
+            MARITAL_STATUS,
+            GENDER,
+            CREATE_DATE,
+            LOYALTY_JOIN_DATE,
+            STORE_KEY,
+            DWH_STATUS,
+            DWH_START_DATE,
+            DWH_END_DATE,
+            DWH_CREATE_DATE
+        )
+    VALUES
+        (
+            SRC.CST_ID,
+            SRC.CST_KEY,
+            SRC.CST_FIRSTNAME,
+            SRC.CST_LASTNAME,
+            SRC.BDATE,
+            SRC.CST_MARITAL_STATUS,
+            SRC.CST_GNDR_derived,
+            SRC.CST_CREATE_DATE,
+            SRC.CST_LOYALTY_JOIN_DATE,
+            SRC.STORE_KEY,
+            SRC.DWH_STATUS,
+            SRC.DWH_START_DATE,
+            SRC.DWH_END_DATE,
+            SRC.DWH_CREATE_DATE
+        );
+    
+    INSERT INTO SALES.GOLD.DIM_CUSTOMER (
+        CUSTOMER_ID,
+        CUSTOMER_NUMBER,
+        FIRST_NAME,
+        LAST_NAME,
+        BIRTH_DATE,
+        MARITAL_STATUS,
+        GENDER,
+        CREATE_DATE,
+        LOYALTY_JOIN_DATE,
+        STORE_KEY,
+        DWH_STATUS,
+        DWH_START_DATE,   
+        DWH_END_DATE,
+        DWH_CREATE_DATE
+    )
+    
+    SELECT 
+        SRC.*
+    FROM 
+        SALES.GOLD.VW_NEW_CUSTOMER_SRC AS SRC
+    LEFT JOIN 
+        SALES.GOLD.DIM_CUSTOMER AS TGT
+        ON TGT.CUSTOMER_ID = SRC.CST_ID AND TGT.DWH_STATUS = SRC.DWH_STATUS
+    WHERE 
+        TGT.CUSTOMER_NUMBER IS NULL; 
+
+    RETURN 'Inserted Current records';
+END;
+$$;
