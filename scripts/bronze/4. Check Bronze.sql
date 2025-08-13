7a6de513a14429326c1af67d7b38f8acd5e57971
Mar San Pedro <127751590+jmmsanpedro@users.noreply.github.com>
2025-08-13 12:58:42 +0800

    Capitalized keywords

--- scripts/bronze/4. Check Bronze.sql
+++ scripts/bronze/4. Check Bronze.sql
@@ -1,11 +1,11 @@
-use role sysadmin;
-use database sales;
+USE ROLE SYSADMIN;
+USE DATABASE SALES;
 
-select * from bronze.crm_cust_info;
-select * from bronze.crm_prd_info;
-select * from bronze.crm_sales_details;
+SELECT * FROM bronze.crm_cust_info;
+SELECT * FROM bronze.crm_prd_info;
+SELECT * FROM bronze.crm_sales_details;
 
-select * from bronze.erp_cust_az12;
-select * from bronze.erp_px_cat_g1v2;
-select * from bronze.erp_stores;
+SELECT * FROM bronze.erp_cust_az12;
+SELECT * FROM bronze.erp_px_cat_g1v2;
+SELECT * FROM bronze.erp_stores;
 
